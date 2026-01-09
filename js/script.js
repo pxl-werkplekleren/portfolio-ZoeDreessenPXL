349e803035e38a8e5fa67637cfd0a466a480ca72
carinezoe <zoe.dreessen@student.pxl.be>
2026-01-09 09:55:34 +0100

    Changed styling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-function ToggleVisibility(number) 
-{
-    const element = document.getElementById("explanation_" + number);
-    element.classList.toggle('hideElement');
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const headings = document.querySelectorAll("article h2");
+
+    headings.forEach(h2 => {
+        h2.addEventListener("click", () => {
+            const content = h2.nextElementSibling;
+            content.classList.toggle("hideElement");
+        });
+    });
+});
